241ff430a7c6b428674bd406db16f1c8bf96bdb0
ivan-hryshko <giver313@gmail.com>
2025-04-26 21:14:42 +0300

    reafctor: move field entity

--- src/modules/fields/field.entity.ts
+++ src/modules/fields/field.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToOne, JoinColumn } from "typeorm"
-import { GameEntity } from "../modules/games/game.entity"
+import { GameEntity } from "../games/game.entity"
+
 @Entity({ name: "fields" })
 export class FieldEntity {
   @PrimaryGeneratedColumn()
