e83599cbc0618af3e46f4be382d1425f6f9e12c9
ivan-hryshko <giver313@gmail.com>
2025-05-05 23:12:54 +0300

    test: fix test

--- src/modules/games/tests/game.test.ts
+++ src/modules/games/tests/game.test.ts
@@ -1,6 +1,30 @@
 
-describe('POST /api/v1/movies', () => {
-  it('should create a movie with emty actors', async () => {
-    expect(true).toBe(false)
+import request from 'supertest'
+import app from '../../../app'
+import AppDataSource from '../../../config/app-data-source'
+
+beforeAll(async () => {
+  if (!AppDataSource.isInitialized) {
+    await AppDataSource.initialize();
+  }
+});
+
+afterAll(async () => {
+  if (AppDataSource.isInitialized) {
+    await AppDataSource.destroy();
+  }
+});
+
+describe('POST /api/v1/game', () => {
+  it('should create a game with', async () => {
+    expect(true).toBe(true)
+
+    const response = await request(app)
+      .post('/api/v1/games/create')
+      // .send(movieData)
+      // .set('Authorization', `${testHelper.getToken()}`)
+
+    expect(response.status).toBe(200)
+
   })
 })
